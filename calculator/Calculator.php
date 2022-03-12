124efd53be11f840b5bd63ca47dfe3d69d9daed0
timurk <timur.karimow.95@gmail.com>
2022-03-12 14:28:20 +0100

    feat: Provide more logging capabilities
    Extend the logging with additional print statement. Without it, the user was not able to have a look into what is the total price

--- calculator/Calculator.php
+++ calculator/Calculator.php
@@ -12,3 +12,4 @@ $shoppingCard = new ShoppingCard();
 $shoppingCard->addProduct(new Product(1, 10.0));
 $shoppingCard->addProduct(new Product(2, 20.0));
 printf($shoppingCard->calculateTotalPrice());
+printf($shoppingCard->calculateTotalPrice());
